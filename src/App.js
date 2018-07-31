01c8da86a4ef59d934b59dc46b0c96343ad9d517
nazariy17 <nazariy2410@gmail.com>
2018-07-31 11:49:14 +0300

    add styles

--- src/App.js
+++ src/App.js
@@ -4,8 +4,12 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-
+      <div className="coder">
+          <div className="row m-row">
+            <div className="column">
+              asdasdda
+            </div>
+          </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
+import './assets/styles/build.css';
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('coder-app'));
 registerServiceWorker();
