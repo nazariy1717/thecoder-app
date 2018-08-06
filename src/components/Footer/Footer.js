5a7319dfeb1ebf65c98fb0896a434ad24a902ff0
nazariy17 <nazariy2410@gmail.com>
2018-08-06 11:11:42 +0300

    dev

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,55 @@
 import React from 'react';
-const footer = props => (
-    <footer>
-        <div className="row m-row align-justify">
-            <div className="column">
+import './footer.scss';
+import logo from './logo-foot.svg';
 
-            </div>
-            <div className="column">
 
+const footer = () => (
+    <footer className="footer">
+        <div className="container">
+            <div className="row m-row align-middle ">
+                <div className="column col-lg-3">
+                    <a href="/">
+                        <img src={logo} alt="the coder logo" title="the coder logo" className="footer__logo"/>
+                    </a>
+                </div>
+                <div className="column col-lg-8">
+                    <div className="row m-row align-spaced">
+                        <div className="column col-lg-4 footer__column">
+                            <span className="footer__txt --title">Адресa/</span>
+                            <span className="footer__txt">м. Львів</span>
+                            <span className="footer__txt">м. Cан-Франциско</span>
+                        </div>
+                        <div className="column col-lg-4 footer__column">
+                            <span className="footer__txt --title">телефон/</span>
+                            <span className="footer__txt">+380 00 000 000</span>
+                            <span className="footer__txt">+380 00 000 000</span>
+                        </div>
+                        <div className="column col-lg-4 footer__column">
+                            <span className="footer__txt --title">e-mail/</span>
+                            <span className="footer__txt">thecoder@gmail.com</span>
+                        </div>
+                    </div>
+                    <ul className="row m-row align-spaced">
+                        <li className="column col-lg-4">
+                            <a href="/" className="footer__txt footer__link" target="_blank" rel="nofollow">
+                                <span>Facebook</span>
+                                <span>Facebook</span>
+                            </a>
+                        </li>
+                        <li className="column col-lg-4">
+                            <a href="/" className="footer__txt footer__link" target="_blank" rel="nofollow">
+                                <span>Instagram</span>
+                                <span>Instagram</span>
+                            </a>
+                        </li>
+                        <li className="column col-lg-4">
+                            <a href="/" className="footer__txt footer__link" target="_blank" rel="nofollow">
+                                <span>Linkedin</span>
+                                <span>Linkedin</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     </footer>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import './styles/app.scss';
+import './assets/styles/app.scss';
 
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
