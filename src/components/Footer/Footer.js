c622d2e61d5d7692c505f963659ccae899b8c9d6
Nazar Kalinchuk <nazariy2410@gmail.com>
2018-09-04 15:08:31 +0300

    dev

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -14,17 +14,17 @@ const footer = () => (
                 </div>
                 <div className="column col-lg-9 col-md-9 col-sm-10 col-xs-12">
                     <div className="row m-row align-spaced footer__row">
-                        <div className="column col-xs-4 footer__column">
+                        <div className="column col-sm-4 col-xs-12 footer__column">
                             <span className="footer__txt --title">Address/</span>
                             <span className="footer__txt">м. Львів</span>
                             <span className="footer__txt">м. Cан-Франциско</span>
                         </div>
-                        <div className="column col-xs-4 footer__column">
+                        <div className="column col-sm-4 col-xs-12 footer__column">
                             <span className="footer__txt --title">phones/</span>
                             <a href="tel:" className="footer__txt">+380 00 000 000</a>
                             <a href="tel:" className="footer__txt">+380 00 000 000</a>
                         </div>
-                        <div className="column col-xs-4 footer__column">
+                        <div className="column col-sm-4 col-xs-12 footer__column">
                             <span className="footer__txt --title">e-mail/</span>
                             <a href="mailto:thecoder@gmail.com" className="footer__txt">thecoder@gmail.com</a>
                         </div>
