a28584df02d6d8e04e444a7efbfbc5d1ae931cdc
nazariy17 <nazariy2410@gmail.com>
2018-10-11 17:19:18 +0300

    dev

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Switch,Route } from 'react-router-dom';
+import UserRoute from "./components/routes/UserRoute";
 
-import Header from './components/elements/Header/Header';
-import Footer from './components/elements/Footer/Footer';
 
 import HomePage from './components/pages/HomePage';
 import BlogPage from './components/pages/BlogPage/BlogPage';
 import PostPage from './components/pages/PostPage/PostPage';
+
+import AdminAuth from './components/admin/AdminAuth/AdminAuth';
 
 import ThanksPage from './components/pages/ThanksPage/ThanksPage';
 import NotFoundPage from './components/pages/NotFoundPage/NotFoundPage';
@@ -14,18 +15,18 @@ import NotFoundPage from './components/pages/NotFoundPage/NotFoundPage';
 
 const App = () =>(
     <div className="coder-app">
-        <Header />
-        <Switch  >
-            <Route exact path="/" component={HomePage} />
-            <Route exact path="/blog" component={BlogPage} />
-            <Route path="/blog/:post" component={PostPage} />
+        <Switch>
 
+            <UserRoute exact path="/" component={HomePage} />
+            <UserRoute exact path="/blog" component={BlogPage} />
+            <UserRoute path="/blog/:post" component={PostPage} />
 
-            <Route path="/thanks" component={ThanksPage} />
-            <Route component={NotFoundPage} />
+            <Route exact path="/coder-admin" component={AdminAuth} />
+
+            <UserRoute path="/thanks" component={ThanksPage} />
+            <UserRoute component={NotFoundPage} />
 
         </Switch>
-        <Footer />
     </div>
 );
 
