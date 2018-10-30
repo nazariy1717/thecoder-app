92766cf3dfca8d569343ed2d0a73cdf062dd658c
nazariy17 <nazariy2410@gmail.com>
2018-10-30 19:02:52 +0200

    dev

---
+++ server/controllers/article.ctrl.js
@@ -0,0 +1,22 @@
+import Article from './../models/Article';
+
+
+module.exports = {
+
+    addArticle: (req, res, next) => {
+        let { text, title, claps, description } = req.body;
+
+    },
+
+    getAll: (req, res, next) => {
+
+    },
+
+    clapArticle: (req, res, next) => {
+
+    },
+
+    getArticle: (req, res, next) => {
+
+    }
+};
---
+++ server/routes/adminAuth.js
@@ -0,0 +1,17 @@
+import articleController from "./../controllers/article.ctrl";
+
+module.exports = (router) => {
+
+    /** get all articles */
+    router.route('/articles').get(articleController.getAll);
+
+    /** add an article */
+    router.route('/article').post(multipartWare, articleController.addArticle);
+
+    /** clap on an article */
+    router.route('/article/clap').post(articleController.clapArticle);
+
+    /** get an article */
+    router.route('/article/:id').get(articleController.getArticle)
+
+};
