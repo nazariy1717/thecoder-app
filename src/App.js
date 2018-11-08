96162077a221f39ef9aa8d02135d7d882f249f85
nazariy17 <nazariy2410@gmail.com>
2018-11-08 15:52:44 +0200

    dev

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import { Switch,Route } from 'react-router-dom';
-
 import HomePage from './components/pages/HomePage';
 import ThanksPage from './components/pages/ThanksPage/ThanksPage';
 // import NotFoundPage from './components/pages/NotFoundPage/NotFoundPage';
-
 
 const App = () => (
     <div className="coder-app">
